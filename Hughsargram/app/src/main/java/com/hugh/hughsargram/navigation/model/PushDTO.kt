e8a1083479c5ffed13b1d12208c968dad1bd291f
psh140 <psh140140@gmail.com>
2020-06-22 15:56:43 +0900

    PushDTO set

---
+++ Hughsargram/app/src/main/java/com/hugh/hughsargram/navigation/model/PushDTO.kt
@@ -0,0 +1,14 @@
+package com.hugh.hughsargram.navigation.model
+
+data class PushDTO(
+    var to : String? = null,
+    var notification : Notification = Notification()
+) {
+    data class Notification(
+        var body : String? = null,
+        var title : String? = null
+    )
+}
+
+//to - push를 받는사람으 토큰아이디
+//body - push 메세지의 주 내용
